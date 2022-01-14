c94dc436277bb2cb623f1dc6c37db62107234354
duclos pierre <duclospi@gmail.com>
2022-01-14 11:10:19 +0100

    swiper terminado, en route vers la prochaine grid

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,27 @@
-console.log("prouit");
-console.log("pasporut");
-let test = true;
+  //-------------//
+  //Swiper config//
+  //-------------//
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    //autoplay
+    autoplay: {
+        delay: 3000,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    //fade
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+});
+
+//-------------//
+//Swiper config//
+//-------------//
